289f9a8b31749a61ae824c8aa58f81c614c0ea4a
yokobot <takuou@gmail.com>
2021-08-07 12:56:52 +0900

    動いたので一旦push

---
+++ README.md
@@ -0,0 +1,13 @@
+# schemaspy
+
+## usage
+
+```
+docker run \
+-e DB_HOST=hostname \
+-e DB_USER=username \
+-e DB_NAME=dbname \
+-e DB_PASS=dbpasswrod \
+-v "$PWD/output:/output" \
+imagename
+```
---
+++ docker/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+[ -d $SCHEMASPY_DRIVERS ] && export DRIVER_PATH=$SCHEMASPY_DRIVERS || export DRIVER_PATH=/drivers_inc/
+echo -n "Using drivers:"
+ls -Ax $DRIVER_PATH | sed -e 's/  */, /g'
+exec java -jar /usr/local/lib/schemaspy/schemaspy*.jar -dp $DRIVER_PATH -o $SCHEMASPY_OUTPUT -t mysql -host ${DB_HOST} -u ${DB_USER} -db ${DB_NAME} -s ${DB_NAME} -port 3306 -p ${DB_PASS}
+aws s3 rm $S3PATH --recursive
+aws s3 cp /output $S3PATH --recursive
